a5f194fa62c4ff2775883a0d6fc025dffb61ec7d
NessMC <contact@nessmc.fr>
2020-05-03 11:37:55 +0200

    🚸 Navbar list open function added.

--- src/js/ness.js
+++ src/js/ness.js
@@ -2,12 +2,22 @@
                   NESS
 ///////////////////////////////////////*/
 
-let   navbar = document.getElementsByClassName('ness-navbar')[0]
+let navbar = document.getElementsByClassName('ness-navbar')[0],
+    menu   = navbar.getElementsByClassName('ness-navbar-menu')[0],
+    button = menu.getElementsByClassName('ness-navbar-button')[0]
 
 window.addEventListener('scroll', () => {
     if (window.pageYOffset > 199) {
         navbar.classList.add('ness-navbar-scrolled')
     } else {
         navbar.classList.remove('ness-navbar-scrolled')
+    }
+})
+
+button.addEventListener('click', () => {
+    if (menu.classList.contains('ness-navbar-opened')) {
+        menu.classList.remove('ness-navbar-opened')
+    } else {
+        menu.classList.add('ness-navbar-opened')
     }
 })
