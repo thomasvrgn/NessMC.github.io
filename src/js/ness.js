8b029fd620bcb2122556ad041eea62e213e1cc72
NessMC <contact@nessmc.fr>
2020-06-11 10:17:20 +0200

    🚸 Background effect added. What's new:  * Three.js lib added.  * VANTA lib added.  * Wave effect set.

--- src/js/ness.js
+++ src/js/ness.js
@@ -1,3 +1,28 @@
 /*///////////////////////////////////////
                   NESS
 ///////////////////////////////////////*/
+
+
+
+document.addEventListener('readystatechange', () => {
+    if (document.readyState === 'complete') {
+        waves()
+    }
+})
+
+function waves () {
+    VANTA.WAVES({
+        el: ".ness-header",
+        mouseControls: false,
+        touchControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0xc0c0c,
+        shininess: 0.00,
+        waveHeight: 6.00,
+        waveSpeed: 0.85,
+        zoom: 0.98
+    })
+}
